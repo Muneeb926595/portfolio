dbd53e705b587d3edc79ff0464397a981f3b1fe1
Muneeb926595 <31837560+Muneeb926595@users.noreply.github.com>
2020-08-30 08:54:13 +0500

    NavLinks borders

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import HomeImg from '../../assets/img/icons8_home.svg';
 import AboutImg from '../../assets/img/icons8_user.svg';
 import ServicesImg from '../../assets/img/icons8_briefcase.svg';
@@ -53,17 +53,15 @@ const navLinks = [
 ]
 
 function Navbar() {
-    const [tab, setTab] = useState(1);
-    console.log(tab)
     return (
         <nav className="navbar navbar-dark fixed-top bg-white shadow off-canvas" data-right-drawer="0" data-open-drawer="1" style={{ width: '90px', paddingTop: 0 }}>
             <div className="container-fluid flex-column">
                 <div className="d-flex justify-content-between brand-line"
                     style={{ width: '124px', marginLeft: '-16px' }}>
-                    <Link onClick={() => { setTab(1) }} className="navbar-brand text-center text-white d-md-flex justify-content-md-center align-items-md-center"
+                    <NavLink className="navbar-brand text-center text-white d-md-flex justify-content-md-center align-items-md-center"
                         to="/"
                         style={{ backgroundColor: '#35ab39', width: '90px', height: '90px', fontSize: '44px', fontFamily: 'Poppins, sans-serif', fontWeight: 900, fontStyle: 'normal', marginRight: '0px' }}>M
-                        </Link>
+                        </NavLink>
                 </div>
                 <ul className="nav navbar-nav text-center d-flex flex-column flex-column drawer-menu"
                     style={{ width: '90px', marginTop: '40px', marginLeft: '-50px' }}>
@@ -71,14 +69,14 @@ function Navbar() {
                         navLinks.map((link) => {
                             return (
                                 <li key={link.id} className="nav-item my-nav-item" role="presentation">
-                                    <Link onClick={() => { setTab(link.id) }} className="nav-link active text-dark d-md-flex justify-content-md-center align-items-md-center"
-                                        to={link.to} style={{ width: '90px', height: '72px' }}>
+                                    <NavLink activeClassName="myActiveLink" className="nav-link text-dark d-md-flex justify-content-md-center align-items-md-center"
+                                        to={link.to} exact style={{ width: '90px', height: '72px' }}>
                                         <div className="d-flex flex-column align-items-center">
                                             <img style={{ width: '20px', height: '20px', marginBottom: '5px' }} src={link.navIcon}
                                                 alt={link.navLabel} />
                                             {link.navTitle}
                                         </div>
-                                    </Link>
+                                    </NavLink>
                                 </li>
                             )
                         })
