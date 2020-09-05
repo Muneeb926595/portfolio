ae7d79f237913e987d3736f8219a5e3baf07c7ce
Muneeb926595 <31837560+Muneeb926595@users.noreply.github.com>
2020-09-05 16:30:16 +0500

    Update Contact.jsx

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -121,7 +121,7 @@ function Contact() {
                         <div className="col-lg-6">
                             <iframe title="Mylocation"
                                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2794.5617723875966!2d73.02075793580819!3d33.59718906080612!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38df95b2d80e9371%3A0x7ef167fd7f4e6143!2sIqra%20fashion!5e0!3m2!1sen!2s!4v1598332596721!5m2!1sen!2s"
-                                width="600" height="100%" frameBorder="0" style={{ border: '0' }} allowFullScreen=""
+                                width="100%" height="100%" frameBorder="0" style={{ border: '0' }} allowFullScreen=""
                                 aria-hidden="false" tabIndex="0"></iframe>
 
                         </div>
