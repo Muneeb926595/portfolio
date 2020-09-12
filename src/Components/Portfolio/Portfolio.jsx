388f78d40788aa42bdfd5a3a126ea165c617c5a2
Muneeb926595 <31837560+Muneeb926595@users.noreply.github.com>
2020-09-12 11:06:02 +0500

    Added New project to portfolio

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -10,81 +10,89 @@ import Pic8 from '../../assets/img/projects/8.png';
 import Pic9 from '../../assets/img/projects/9.png';
 import Pic0 from '../../assets/img/projects/0.png';
 import Pic11 from '../../assets/img/projects/11.png';
+import Pic12 from '../../assets/img/projects/12.png';
 
 
 const myProjects = [
     {
         id: 1,
+        image: Pic12,
+        projectName: "Facebook Messenger Clone",
+        website: "https://facebook-messanger-clone-e19b4.web.app/",
+        github: "https://github.com/Muneeb926595/facebook-messanger-clone"
+    },
+    {
+        id: 2,
         image: Pic11,
         projectName: "SpaceX App",
         website: "http://space-world-app.surge.sh/",
         github: "https://github.com/Muneeb926595/space-x-app"
     },
     {
-        id: 2,
+        id: 3,
         image: Pic3,
         projectName: "Responsive Website",
         website: "http://muneeb-portfolio.surge.sh/",
         github: "https://github.com/Muneeb926595/my-portfolio"
     },
     {
-        id: 3,
+        id: 4,
         image: Pic7,
         projectName: "Covid 19 Teacker",
         website: "http://muneeb-covid19-tracker.surge.sh/",
         github: "https://github.com/Muneeb926595/covid-19-tracker"
     },
     {
-        id: 4,
+        id: 5,
         image: Pic2,
         projectName: "Quiz Application",
         website: "http://muneeb-quiz-app.surge.sh/",
         github: "https://github.com/Muneeb926595/my-quizapp"
     },
     {
-        id: 5,
+        id: 6,
         image: Pic9,
         projectName: "Expence Tracker App",
         website: "http://muneeb-expence-tracker-app.surge.sh/",
         github: "https://github.com/Muneeb926595/react-expence-tracker-app"
     },
     {
-        id: 6,
+        id: 7,
         image: Pic4,
         projectName: "Todo Application",
         website: "http://muneeb-todo-app.surge.sh/",
         github: "https://github.com/Muneeb926595/todo-app"
     },
     {
-        id: 7,
+        id: 8,
         image: Pic8,
         projectName: "Nike Shoes Store",
         website: "http://muneeb-nike-shoes.surge.sh/",
         github: "https://github.com/Muneeb926595/simple-commerce-app"
     },
     {
-        id: 8,
+        id: 9,
         image: Pic1,
         projectName: "Timer App",
         website: "http://muneeb-timer-app.surge.sh",
         github: "https://github.com/Muneeb926595/timer-app-tdd"
     },
     {
-        id: 9,
+        id: 10,
         image: Pic6,
         projectName: "Cortex Website Clone",
         website: "http://muneeb-cotex-clone.surge.sh/",
         github: "https://github.com/Muneeb926595/cortex-website-clone"
     },
     {
-        id: 10,
+        id: 11,
         image: Pic5,
         projectName: "Alice Running App",
         website: "http://muneeb-project4a.surge.sh/",
         github: "https://github.com/Muneeb926595/react-animations-alic-running"
     },
     {
-        id: 11,
+        id: 12,
         image: Pic0,
         projectName: "Counter App",
         website: "http://muneeb_second_react_app.surge.sh/",
